2f47919e51ee50a9729fa5c36eea58e92de7cd55
Amin <32376329+amin2018@users.noreply.github.com>
2023-12-11 22:47:06 -0500

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "service_endpoint" {
-  description = "The endpoint of the Kubernetes service"
-  value       = kubernetes_service.hello_world.status.0.load_balancer.0.ingress.0.ip
-}
-
-output "sql_connection_name" {
-  description = "The connection name of the Cloud SQL instance"
-  value       = google_sql_database_instance.default.connection_name
-}
